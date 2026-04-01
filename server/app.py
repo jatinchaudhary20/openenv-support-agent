cb35aa4aedaf37ba55769b863aedb5e531ea99e3
Jatin Chaudhary <116703023+jatinchaudhary20@users.noreply.github.com>
2026-04-01 07:47:33 +0530

    fix python version for openenv

---
+++ server/app.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+sys.path.append(os.getcwd())
+
+from fastapi import FastAPI
+from env.support_env import SupportEnv
+from env.models import Action
+
+app = FastAPI()
+
+env = SupportEnv()
+
+
+@app.get("/")
+def root():
+    return {"message": "API running"}
+
+
+@app.post("/reset")
+def reset(task: str = "easy"):
+    state = env.reset(task)
+    return state.dict()
+
+
+@app.post("/step")
+def step(action: str):
+    act = Action(action_str=action)
+    state, reward, done, _ = env.step(act)
+    return {
+        "state": state.dict(),
+        "reward": reward,
+        "done": done
+    }
+
+
+@app.get("/state")
+def get_state():
+    return env.state.dict()
